5dc5bc6531c088c05e42f60dcabc01a40b26000f
Ivan Pu <ivanpu@gl-login1.arc-ts.umich.edu>
2021-03-20 22:29:44 -0400

    can run single machine training

--- byteps.sh
+++ byteps.sh
@@ -1,12 +1,15 @@
+
+nvidia-smi
+
 # now you are in docker environment
-export NVIDIA_VISIBLE_DEVICES=0,1,2,3  # gpus list
+export NVIDIA_VISIBLE_DEVICES=0  # gpus list
 export DMLC_WORKER_ID=0 # your worker id
 export DMLC_NUM_WORKER=1 # one worker
 export DMLC_ROLE=worker
 
 # the following value does not matter for non-distributed jobs
 export DMLC_NUM_SERVER=1
-export DMLC_PS_ROOT_URI=10.0.0.1
-export DMLC_PS_ROOT_PORT=1234
+export DMLC_PS_ROOT_URI=15.0.0.3
+export DMLC_PS_ROOT_PORT=7214
 
-bpslaunch python3 /usr/local/byteps/example/pytorch/benchmark_byteps.py --model resnet50 --num-iters 1000000
+bpslaunch python3 /usr/local/byteps/example/pytorch/benchmark_byteps.py --model resnet50 --num-iters 100
--- submit.sh
+++ submit.sh
@@ -4,22 +4,23 @@
 #“#SBATCH” directives that convey submission options:
 
 #SBATCH --job-name=test
-#SBATCH --mail-user=zwq@umich.edu
+#SBATCH --mail-user=ivanpu@umich.edu
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --error=/home/%u/error_%x-%j.log
 #SBATCH --cpus-per-task=1
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --gpus-per-node=1
-#SBATCH --mem-per-cpu=1000m 
+#SBATCH --mem-per-cpu=32768m 
 #SBATCH --partition=gpu
 #SBATCH --output=/home/%u/%x-%j.log
 
 # The application(s) to execute along with its input arguments and options:
 # module load cuda/10.0.130
 module load singularity
-nvidia-smi
-singularity exec test3.simg /home/zwq/EECS598_Proj/byteps.sh
+
+
+singularity exec --nv bytepsimage.simg /home/ivanpu/EECS598_Proj/byteps.sh
 
 /bin/hostname
 sleep 60
